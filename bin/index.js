96de1c6a058067216d30e4064fe23ec1a72a241b
Erik Xavier <erik.xam@gmail.com>
2019-05-21 16:59:58 -0300

    Migrated CLI lib module to caporal.js

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const prog = require('caporal')
-const actions= require('../actions')
+const actions= require('../src/actions')
 
 prog
     .bin('shardus')
@@ -9,14 +9,15 @@ prog
     // init command, to create a new sample project
     .command('init', 'Creates a sample shardus network project')
     .argument('<appName>', 'Project name')
+    .option('--no-install', 'Do not run npm install on the new sample project folder')
     .action(actions.init)
 
-    // start launcher tool
+    // start launcher tool - TODO
     .command('launcher', 'Starts the shardus network node launcher tool')
     .argument('[appName]', 'Shardus network project base dir')
     .action(actions.launcher)
 
-    // executes get-logs tool
+    // executes get-logs tool - TODO
     .command('get-logs', 'Fetch the logs from a shardus network')
     .argument('<nodeUrl>', 'URL from any node on the network')
     .action(actions.getLogs)
