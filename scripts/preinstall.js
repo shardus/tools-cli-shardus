6ecf270298eae3dfe42eaaf415c3132edbef83f7
asyed94 <asyed94@gmail.com>
2019-08-15 13:15:29 -0500

    Changes preinstall script to make sure node version within 10.x.x and npm version within 6.x.x

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -1,19 +1,18 @@
 const { execSync } = require('child_process')
-const compare = require('./semver-compare')
 
 let errored = false
 
 // Check node version >= 10.16.2
 const nodeVersion = process.versions.node
-if (compare(nodeVersion, '10.16.2') < 0) {
-  console.error('\x1b[31m%s\x1b[0m', `Node.js version ${nodeVersion} is not supported. Please use a version >= 10.16.2`)
+if (nodeVersion.split('.')[0] !== '10') {
+  console.error('\x1b[31m%s\x1b[0m', `Node.js version ${nodeVersion} is not supported. Please use a version within 10.x.x`)
   errored = true
 }
 
 // Check npm version >= 6.9.0
 const npmVersion = execSync('npm --version', { encoding: 'utf8' }).trim()
-if (compare(npmVersion, '6.9.0') < 0) {
-  console.error('\x1b[31m%s\x1b[0m', `npm version ${npmVersion} is not supported. Please use a version >= 6.9.0`)
+if (npmVersion.split('.')[0] !== '6') {
+  console.error('\x1b[31m%s\x1b[0m', `npm version ${npmVersion} is not supported. Please use a version within 6.x.x`)
   errored = true
 }
 
