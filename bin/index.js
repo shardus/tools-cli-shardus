c70bf0f3854e23d2461ebd46bbb6c4f84f531baf
asyed94 <asyed94@gmail.com>
2020-02-04 13:21:20 -0600

    Makes bin/index.js get version number from package.json

--- bin/index.js
+++ bin/index.js
@@ -4,11 +4,12 @@ const prog = require('caporal')
 const shardusInit = require('shardus-init')
 const shardusNetwork = require('shardus-network')
 const shardusDebug = require('shardus-debug')
+const packageJson = require('../package.json')
 
 prog
   .bin('shardus')
   .name('Shardus CLI')
-  .version('1.0.0')
+  .version(packageJson.version)
 
 for (const command in shardusInit.register) shardusInit.register[command](prog)
 for (const command in shardusNetwork.register) shardusNetwork.register[command](prog, 'network')
