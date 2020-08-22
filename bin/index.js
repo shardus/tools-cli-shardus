e1fc08cdbafc0c4d1588c60b5aa31070b1cddac2
kshifflett <kyle@shardus.com>
2020-08-21 20:16:11 -0500

    Changed the namespace so that shardus network doesn't need to be typed when running network commands

--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,7 @@ prog
   .version(packageJson.version)
 
 for (const command in shardusInit.register) shardusInit.register[command](prog)
-for (const command in shardusNetwork.register) shardusNetwork.register[command](prog, 'network')
+for (const command in shardusNetwork.register) shardusNetwork.register[command](prog)
 for (const command in shardusDebug.register) shardusDebug.register[command](prog, 'debug')
 
 prog.parse(process.argv)
