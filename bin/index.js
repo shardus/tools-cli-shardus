5cb4946e8da0b7bb226004bc31fd6e44ceba2dbc
Aaron Sullivan <aasullivan1618@gmail.com>
2022-02-18 17:32:34 -0800

    Deprecate init
    This is part of the push for
    https://www.notion.so/shardus/a0e84bf31b97437eac5747d7940fb1c3?v=3bb4c72041194bb08a9fd5b3fcadb659&p=c19507d03e7d4386944120fad6334053
    
    This function hasn't worked for a little while, and when we talked about
    updating it, we realized it'd be better to goodnight it entirely, as you
    can achieve the same functionality basically with git clone.

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 const prog = require('caporal')
-const shardusInit = require('shardus-init')
 const shardusNetwork = require('shardus-network')
 const shardusDebug = require('shardus-debug')
 const packageJson = require('../package.json')
@@ -11,7 +10,6 @@ prog
   .name('Shardus CLI')
   .version(packageJson.version)
 
-for (const command in shardusInit.register) shardusInit.register[command](prog)
 for (const command in shardusNetwork.register) shardusNetwork.register[command](prog)
 for (const command in shardusDebug.register) shardusDebug.register[command](prog, 'debug')
 
